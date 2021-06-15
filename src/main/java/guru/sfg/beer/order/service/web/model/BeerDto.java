396f7a3c0dd680f18c0f1f3c537b00c94cb8ceb1
sasa.devic <sasa.dvc@gmail.com>
2021-06-15 17:20:20 +0200

    Enhance Order Response with Beer Information

---
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerDto.java
@@ -0,0 +1,55 @@
+package guru.sfg.beer.order.service.web.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto implements Serializable {
+
+    static final long serialVersionUID = 8_368_371_261_736_687_063L;
+
+    @Null // id not coming in
+    private UUID id;
+
+    @Null
+    private Integer version;
+
+    @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    private OffsetDateTime createdDate;
+
+    @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
+    private String beerName;
+
+    @NotNull
+    private String upc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @Positive
+    @NotNull
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+}
